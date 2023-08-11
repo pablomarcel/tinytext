a2ed3a5aa1a1db23aefcd01e8086df279567ff4f
pmarcel-uw <pmarcel@uw.edu>
2023-08-11 00:10:25 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, url_for, redirect
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import nltk
+import os
+
+# Ensure nltk resources are downloaded
+# nltk.download('punkt')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development_key'  # Use a more secure key in production
+
+# Load the text
+with open('static/divine_comedy.txt', 'r', encoding="utf8") as file:
+    documents = file.readlines()
+
+
+# Custom tokenizer for the TfidfVectorizer
+def tokenize(text):
+    tokens = [word for word in nltk.word_tokenize(text) if len(word) > 1 and not word.isnumeric()]
+    return tokens
+
+
+vectorizer = TfidfVectorizer(stop_words='english', tokenizer=tokenize)
+tfidf_matrix = vectorizer.fit_transform(documents)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        query = request.form['query']
+        if query:
+            tfidf_query = vectorizer.transform([query])
+            cosine_similarities = linear_kernel(tfidf_query, tfidf_matrix).flatten()
+            related_docs_indices = cosine_similarities.argsort()[:-5:-1]
+
+            results = [documents[i] for i in related_docs_indices]
+            return render_template("results.html", query=query, results=zip(range(1, len(results) + 1), results))
+        else:
+            return redirect(url_for('index'))
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
